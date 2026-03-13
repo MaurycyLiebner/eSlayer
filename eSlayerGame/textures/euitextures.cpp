d6a3335c2241a2662033bd8e8c5a6e587beaa996
maurycyliebner <maurycy.liebner@gmail.com>
2026-03-13 16:11:54 +0100

    Remove orb texture loading.

--- eSlayerGame/textures/euitextures.cpp
+++ eSlayerGame/textures/euitextures.cpp
@@ -1,15 +1,8 @@
 #include "euitextures.h"
 
-#include "espriteloader.h"
-
-std::shared_ptr<eTexture> eUITextures::sOrb;
-std::shared_ptr<eTexture> eUITextures::sOrbFront;
 bool eUITextures::sLoaded = false;
 
 void eUITextures::sLoad(SDL_Renderer* const r) {
     if(sLoaded) return;
     sLoaded = true;
-    eSpriteLoader loader("Textures", "ui/orb", r);
-    sOrb = loader.load(0);
-    sOrbFront = loader.load(1);
 }
--- eSlayerGame/textures/euitextures.h
+++ eSlayerGame/textures/euitextures.h
@@ -1,14 +1,11 @@
 #ifndef EUITEXTURES_H
 #define EUITEXTURES_H
 
-#include "../textures/etexture.h"
+class SDL_Renderer;
 
 class eUITextures {
 public:        
     static void sLoad(SDL_Renderer * const r);
-
-    static std::shared_ptr<eTexture> sOrb;
-    static std::shared_ptr<eTexture> sOrbFront;
 private:
     static bool sLoaded;
 };
